37d9f75da7d38ee59bc80628b5c04e60ba288885
Igo Pinheiro <igo.pinheiro1@gmail.com>
2023-06-23 09:31:34 -0400

    Primeiro Commit

---
+++ ApiAuth/Controllers/LoginController.cs
@@ -0,0 +1,48 @@
+﻿using ApiAuth.Models;
+using ApiAuth.Repository;
+using ApiAuth.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiAuth.Controllers;
+
+[ApiController]
+[Route("v1")]
+public class LoginController : ControllerBase
+{
+    [HttpPost]
+    [Route("login")]
+    public async Task<ActionResult<dynamic>> AuthenticateAsync([FromBody]User model)
+    {
+        var user = UserRepository.Get(model.UserName,model.Password);
+
+        if (user == null)
+            return NotFound(new { message = "Usuário ou senha inválido!"});
+
+        var token = TokenService.GenerateToken(user);
+
+        user.Password = string.Empty;
+
+        return new { user = user, token = token};
+    }
+
+    [HttpGet]
+    [Route("anonymous")]
+    [AllowAnonymous]
+    public string Anonymous() => "Anônimo";
+
+    [HttpGet]
+    [Route("authenticated")]
+    [Authorize]
+    public string Authenticated() => String.Format("Autenticado - {0}", User.Identity.Name);
+
+    [HttpGet]
+    [Route("employee")]
+    [Authorize(Roles = "employee,manager")]
+    public string Employee() => "Funcionário";
+
+    [HttpGet]
+    [Route("manager")]
+    [Authorize(Roles = "manager")]
+    public string Manager() => "Gerente";
+}
---
+++ ApiAuth/Repository/UserRepository.cs
@@ -0,0 +1,27 @@
+﻿using ApiAuth.Models;
+using System.Reflection.Metadata.Ecma335;
+
+namespace ApiAuth.Repository
+{
+    public static class UserRepository
+    {
+        public static User? Get(string username, string password)
+        {
+            var users = new List<User>()
+            {
+               new User { Id = 1, UserName = "batman", Password = "123", Role="manager"},
+            new User { Id = 2, UserName = "robin", Password = "123", Role = "employee" }
+
+            };
+
+            //return users
+            //    .Where(x => x.UserName.ToLower()== username.ToLower() && x.Password == password)
+            //    .FirstOrDefault();
+
+            return users
+                .FirstOrDefault(u=> 
+                   u.UserName==username
+                   && u.Password == password);
+        }
+    }
+}
